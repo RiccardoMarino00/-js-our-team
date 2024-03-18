44ee643e76995016d92c4a83d0e872dbd36eb7c2
RiccardoMarino00 <riccardo.marino09@gmail.com>
2024-03-18 16:35:39 +0100

    added: style (card)

--- js/app.js
+++ js/app.js
@@ -4,6 +4,9 @@ const divInfoUsers3 = document.getElementById("content3");
 const divInfoUsers4 = document.getElementById("content4");
 const divInfoUsers5 = document.getElementById("content5");
 const divInfoUsers6 = document.getElementById("content6");
+// const imgUser = document.querySelector(".img");
+// const nameUser = document.querySelector(".content-name");
+// const roleUser = document.querySelector(".content-role");
 // Utenti 
 const wayne = gnerateUser ("Wayne Barnett", "Founder & CEO", "wayne-barnett-founder-ceo.jpg");
 const angela = gnerateUser ("Angela Caroll", "Chief Editor", "angela-caroll-chief-editor.jpg");
@@ -20,35 +23,41 @@ for (let i = 0; i < users.length; i++){
     const currentUser = users[i];
     console.log(currentUser.name, currentUser.role, currentUser.photo)
     // Creo un nuovo elemento per stampare nel DOM le info degli utenti 
+    // imgUser.innerHTML = `<img src="./img/wayne-barnett-founder-ceo.jpg" class="card-img-top" alt="">`
+    // nameUser.innerHTML = `${users[i].name}`
+    // roleUser.innerHTML = `${users[i].role}`
+
+
+
     divInfoUsers.innerHTML = `
-    IL nome è:  ${users[0].name} &nbsp; &nbsp; &nbsp;
-    Il ruolo è:  ${users[0].role} &nbsp; &nbsp; &nbsp;
-    la foto è: <img src="./img/wayne-barnett-founder-ceo.jpg" alt=""> &nbsp; &nbsp; &nbsp;
+    ${users[0].name} &nbsp; &nbsp; &nbsp;
+    ${users[0].role} &nbsp; &nbsp; &nbsp;
+    <img src="./img/wayne-barnett-founder-ceo.jpg" alt=""> &nbsp; &nbsp; &nbsp;
     `
     divInfoUsers2.innerHTML = `
-    IL nome è:  ${users[1].name} &nbsp; &nbsp; &nbsp;
-    Il ruolo è:  ${users[1].role} &nbsp; &nbsp; &nbsp;
-    la foto è: <img src="./img/angela-caroll-chief-editor.jpg" alt=""> &nbsp; &nbsp; &nbsp;
+    ${users[1].name} &nbsp; &nbsp; &nbsp;
+    ${users[1].role} &nbsp; &nbsp; &nbsp;
+    <img src="./img/angela-caroll-chief-editor.jpg" alt=""> &nbsp; &nbsp; &nbsp;
     `
     divInfoUsers3.innerHTML = `
-    IL nome è:  ${users[2].name} &nbsp; &nbsp; &nbsp;
-    Il ruolo è:  ${users[2].role} &nbsp; &nbsp; &nbsp;
-    la foto è: <img src="./img/walter-gordon-office-manager.jpg" alt=""> &nbsp; &nbsp; &nbsp;
+    ${users[2].name} &nbsp; &nbsp; &nbsp;
+    ${users[2].role} &nbsp; &nbsp; &nbsp;
+    <img src="./img/walter-gordon-office-manager.jpg" alt=""> &nbsp; &nbsp; &nbsp;
     `
     divInfoUsers4.innerHTML = `
-    IL nome è:  ${users[3].name} &nbsp; &nbsp; &nbsp;
-    Il ruolo è:  ${users[3].role} &nbsp; &nbsp; &nbsp;
-    la foto è: <img src="./img/angela-lopez-social-media-manager.jpg" alt=""> &nbsp; &nbsp; &nbsp;
+    ${users[3].name} &nbsp; &nbsp; &nbsp;
+     ${users[3].role} &nbsp; &nbsp; &nbsp;
+    <img src="./img/angela-lopez-social-media-manager.jpg" alt=""> &nbsp; &nbsp; &nbsp;
     `
     divInfoUsers5.innerHTML = `
-    IL nome è:  ${users[4].name} &nbsp; &nbsp; &nbsp;
-    Il ruolo è:  ${users[4].role} &nbsp; &nbsp; &nbsp;
-    la foto è: <img src="./img/scott-estrada-developer.jpg" alt=""> &nbsp; &nbsp; &nbsp;
+     ${users[4].name} &nbsp; &nbsp; &nbsp;
+    ${users[4].role} &nbsp; &nbsp; &nbsp;
+    <img src="./img/scott-estrada-developer.jpg" alt=""> &nbsp; &nbsp; &nbsp;
     `
     divInfoUsers6.innerHTML = `
-    IL nome è:  ${users[5].name} &nbsp; &nbsp; &nbsp;
-    Il ruolo è:  ${users[5].role} &nbsp; &nbsp; &nbsp;
-    la foto è: <img src="./img/barbara-ramos-graphic-designer.jpg" alt=""> &nbsp; &nbsp; &nbsp;
+    ${users[5].name} &nbsp; &nbsp; &nbsp;
+    ${users[5].role} &nbsp; &nbsp; &nbsp;
+    <img src="./img/barbara-ramos-graphic-designer.jpg" alt=""> &nbsp; &nbsp; &nbsp;
     `
 }
 
