3387b1d5de24068557dd36bc2c57e3ddb4f25384
RiccardoMarino00 <riccardo.marino09@gmail.com>
2024-03-18 19:32:25 +0100

    Fixed: innerHTML

--- js/app.js
+++ js/app.js
@@ -4,9 +4,9 @@ const divInfoUsers3 = document.getElementById("content3");
 const divInfoUsers4 = document.getElementById("content4");
 const divInfoUsers5 = document.getElementById("content5");
 const divInfoUsers6 = document.getElementById("content6");
-// const imgUser = document.querySelector(".img");
-// const nameUser = document.querySelector(".content-name");
-// const roleUser = document.querySelector(".content-role");
+const imgUser = document.querySelector(".img");
+const nameUser = document.querySelector(".name");
+const roleUser = document.querySelector(".job");
 // Utenti 
 const wayne = gnerateUser ("Wayne Barnett", "Founder & CEO", "wayne-barnett-founder-ceo.jpg");
 const angela = gnerateUser ("Angela Caroll", "Chief Editor", "angela-caroll-chief-editor.jpg");
@@ -23,47 +23,14 @@ for (let i = 0; i < users.length; i++){
     const currentUser = users[i];
     console.log(currentUser.name, currentUser.role, currentUser.photo)
     // Creo un nuovo elemento per stampare nel DOM le info degli utenti 
-    // imgUser.innerHTML = `<img src="./img/wayne-barnett-founder-ceo.jpg" class="card-img-top" alt="">`
-    // nameUser.innerHTML = `${users[i].name}`
-    // roleUser.innerHTML = `${users[i].role}`
-
-
-
-    divInfoUsers.innerHTML = `
-    ${users[0].name} &nbsp; &nbsp; &nbsp;
-    ${users[0].role} &nbsp; &nbsp; &nbsp;
-    <img src="./img/wayne-barnett-founder-ceo.jpg" class="rounded-3" alt=""> &nbsp; &nbsp; &nbsp;
-    `
-    divInfoUsers2.innerHTML = `
-    ${users[1].name} &nbsp; &nbsp; &nbsp;
-    ${users[1].role} &nbsp; &nbsp; &nbsp;
-    <img src="./img/angela-caroll-chief-editor.jpg" class="rounded-3" alt=""> &nbsp; &nbsp; &nbsp;
-    `
-    divInfoUsers3.innerHTML = `
-    ${users[2].name} &nbsp; &nbsp; &nbsp;
-    ${users[2].role} &nbsp; &nbsp; &nbsp;
-    <img src="./img/walter-gordon-office-manager.jpg" class="rounded-3" alt=""> &nbsp; &nbsp; &nbsp;
-    `
-    divInfoUsers4.innerHTML = `
-    ${users[3].name} &nbsp; &nbsp; &nbsp;
-     ${users[3].role} &nbsp; &nbsp; &nbsp;
-    <img src="./img/angela-lopez-social-media-manager.jpg" class="rounded-3" alt=""> &nbsp; &nbsp; &nbsp;
-    `
-    divInfoUsers5.innerHTML = `
-     ${users[4].name} &nbsp; &nbsp; &nbsp;
-    ${users[4].role} &nbsp; &nbsp; &nbsp;
-    <img src="./img/scott-estrada-developer.jpg" class="rounded-3" alt=""> &nbsp; &nbsp; &nbsp;
-    `
-    divInfoUsers6.innerHTML = `
-    ${users[5].name} &nbsp; &nbsp; &nbsp;
-    ${users[5].role} &nbsp; &nbsp; &nbsp;
-    <img src="./img/barbara-ramos-graphic-designer.jpg" alt="" class="rounded-3"> &nbsp; &nbsp; &nbsp;
+    divInfoUsers.innerHTML += `
+    <div  class="text-center  border border-warning-subtle border-4 rounded-4 py-2 bg-warning-subtle carta">
+                    <div class="img "><img class="rounded-3" src="./img/${users[i].photo}" alt=""></div>
+                    <h3 class="name py-2">${users[i].name}</h3>
+                    <div class="job py-2 fs-5">${users[i].role}</div>
+                </div>
     `
 }
-
-
-
-
 
 
 // Creo la funzione per generarmi gli utenti
